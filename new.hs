78bc3d748d48ec5b7b87ddad40ca5abbd9656376
Birdy-hw <kyle.dsouza.official@gmail.com>
2024-10-14 18:27:03 -0400

    Update new.hs

--- new.hs
+++ new.hs
@@ -1,24 +1,18 @@
-import FibStateful
+import Control.Monad.State
 
--- | fib function that uses FibStateful to calculate Fibonacci efficiently
-fib :: Int -> FibStateful Integer
-fib 0 oldState = (oldState, 0)
-fib 1 oldState = (oldState, 1)
-fib n oldState = 
-    let (newState1, fib1) = getPreviousNumber oldState
-        (newState2, fib2) = getPreviousPreviousNumber oldState
-        newFib = fib1 + fib2
-        (newState3, _) = setPreviousPreviousNumber fib1 newState2
-        (finalState, _) = setPreviousNumber newFib newState3
-    in (finalState, newFib)
+fibAux :: Int -> State (Integer, Integer) Integer
+fibAux 0 = do
+  (a, _) <- get
+  return a
+fibAux n = do
+    (a, b) <- get
+    put (b, a + b)
+    fibAux (n - 1)
 
--- | fibHelper is a helper function that starts with the initial state
-fibHelper :: Int -> FibState -> (FibState, Integer)
-fibHelper n initialState = fib n initialState
-
--- | The main function that sets up the initial state and prints the result of fib
+fib :: Int -> Integer
+fib n = evalState (fibAux n) (0, 1)
+    
 main :: IO ()
 main = do
-    let initialState = FibState { previousNumber = 1, previousPreviousNumber = 0, currentNumber = 0 }
-    let (finalState, result) = fibHelper 50 initialState
-    print result
+    putStr("The fibonacci of 100 is: ")
+    print $ fib 100
